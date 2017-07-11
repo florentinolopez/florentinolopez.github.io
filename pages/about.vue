f8c3c07ae80df0ae370770b2b33209118b511a4f
Florentino López Carrete <florentino.lopez@gmail.com>
2017-07-11 20:46:13 +0200

    Create about.vue

---
+++ pages/about.vue
@@ -0,0 +1,27 @@
+<template>
+  <section class="container">
+    <div>
+      <logo/>
+      <h1 class="title">
+        Florentino López
+      </h1>
+      <h2 class="subtitle">
+        About page
+      </h2>
+      <h3>Powered by Vue, Nuxt and Node.</h3>
+    </div>
+  </section>
+</template>
+
+<script>
+import Logo from '~components/Logo.vue'
+export default {
+  components: {
+    Logo
+  }
+}
+</script>
+
+<style>
+
+</style>
