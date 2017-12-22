a1c3688f8365decbfb1ebbcea82c3035553cf8b0
Florentino López Carrete <florentino.lopez@gmail.com>
2017-12-22 20:12:30 +0100

    Delete 2.nuxt.bundle.ec08ae8e37ea0c54465d.js

--- _nuxt/2.nuxt.bundle.ec08ae8e37ea0c54465d.js
+++
@@ -1,1 +0,0 @@
-webpackJsonp([2],{145:function(n,t,e){var r=e(17)(e(148),e(153),null,null,null);n.exports=r.exports},148:function(n,t){},153:function(n,t){n.exports={render:function(){var n=this,t=n.$createElement,e=n._self._c||t;return e("div",[e("b-alert",{attrs:{show:""}},[n._v("\n    About page\n  ")]),e("nuxt-link",{attrs:{to:"/"}},[n._v("Link to Home")])],1)},staticRenderFns:[]}}});
